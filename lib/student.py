922bb0aad4efc582c4103f3f47a3b54149cb17f0
mendelr20 <95175747+mendelr20@users.noreply.github.com>
2023-09-05 15:01:17 -0400

    done

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,11 @@ from user import User
 
 class Student(User):
     
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.knowledge = []
+    
+    def learn(self, new_knowledge):
+        if isinstance(new_knowledge, str):
+            self.knowledge.append(new_knowledge)
